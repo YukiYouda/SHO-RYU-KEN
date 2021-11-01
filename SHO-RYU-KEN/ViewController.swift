c190e9545a8bf721e4db23988352becfd51b8f1b
YukiYouda <youda0518@yahoo.co.jp>
2021-11-02 08:51:04 +0900

    アプリ完成！

--- SHO-RYU-KEN/ViewController.swift
+++ SHO-RYU-KEN/ViewController.swift
@@ -8,12 +8,31 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var charaImage: UIImageView!
+    
+    var imageArrayAttack : Array<UIImage> = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        while let attackImage = UIImage(named: "attak\(imageArrayAttack.count+1)") {
+            imageArrayAttack.append(attackImage)
+        }
     }
-
-
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    @IBAction func attackBtn(_ sender: Any) {
+        
+        charaImage.animationImages = imageArrayAttack
+                
+        charaImage.animationDuration = 1
+                charaImage.animationRepeatCount = 1
+                
+                charaImage.startAnimating()
+    }
+    
 }
-
